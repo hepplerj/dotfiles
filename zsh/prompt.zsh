14f2a9c863d3c4ce1f5d7b0acedb7d55f88ed62e
Jason Heppler <jason.heppler@gmail.com>
2013-04-22 14:19:44 -0700

    moar updates

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,48 +1,96 @@
-# setup
- 
-autoload colors; colors;
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
-setopt prompt_subst
- 
-# prompt
- 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%}%{$fg[green]%}["
-ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}*%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
- 
-# show git branch/tag, or name-rev if on detached head
-parse_git_branch() {
-  (command git symbolic-ref -q HEAD || command git name-rev --name-only --no-undefined --always HEAD) 2>/dev/null
+autoload colors zsh/terminfo && colors
+# cheers, @ehrenmurdick
+# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+
+
+git_branch() {
+  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
- 
-# show red star if there are uncommitted changes
-parse_git_dirty() {
-  if command git diff-index --quiet HEAD 2> /dev/null; then
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+
+git_dirty() {
+  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  if [[ $st == "" ]]
+  then
+    echo ""
   else
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+    if [[ $st == "nothing to commit (working directory clean)" ]]
+    then
+      echo "%{$fg[green]%}$(git_prompt_info)%{$reset_color%}"
+    else
+      echo "%{$fg[red]%}$(git_prompt_info)%{$reset_color%}"
+    fi
   fi
 }
- 
-# if in a git repo, show dirty indicator + git branch
-git_custom_status() {
-  local git_where="$(parse_git_branch)"
-  [ -n "$git_where" ] && echo "$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_PREFIX${git_where#(refs/heads/|tags/)}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+
+git_prompt_info () {
+ ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+ echo "${ref#refs/heads/}"
 }
- 
-# show current rbenv version if different from rbenv global
-rbenv_version_status() {
-  local ver=$(rbenv version-name)
-  [ "$(rbenv global)" != "$ver" ] && echo "[$ver]"
+
+unpushed () {
+  /usr/bin/git cherry -v @{upstream} 2>/dev/null
 }
- 
-# put fancy stuff on the right
-if which rbenv &> /dev/null; then
-  RPS1='$(git_custom_status)%{$fg[red]%}$(rbenv_version_status)%{$reset_color%} $EPS1'
-else
-  RPS1='$(git_custom_status) $EPS1'
-fi
- 
-# basic prompt on the left
-PROMPT='%{$fg[cyan]%}%~% %(?.%{$fg[green]%}.%{$fg[red]%})%B$%b '
+
+need_push () {
+  if [[ $(unpushed) == "" ]]
+  then
+    echo ""
+  else
+    echo ":%{$fg[magenta]%}push%{$reset_color%}"
+  fi
+}
+
+rb_prompt(){
+  if $(which rbenv &> /dev/null)
+  then
+    rb_v=$(rbenv version |awk '{print $1}')
+    if [[ $rb_v  == $(rbenv global) ]]
+    then
+      echo ""
+    else
+      echo "%{$fg[yellow]%}$rb_v%{$reset_color%}"
+    fi
+  else
+    echo ""
+  fi
+  }
+
+# This keeps the number of todos always available the right hand side of 
+# my command line.
+# todo(){
+#   if $(which todo.sh &> /dev/null)
+#   then
+#     total_todos=$(echo $(todo.sh ls | wc -l))
+#     pri_A=$(echo $(todo.sh lsp A | wc -l))
+#     let today_todos=pri_A-2
+#     if [ $total_todos != 0 ]
+#     then
+#       echo "T:$today_todos"
+#     else
+#       echo ""
+#     fi
+#   else
+#     echo ""
+#   fi
+# }
+
+directory_name(){
+  echo "%{$fg[blue]%}%~%{$reset_color%}"
+}
+
+# user_machine(){
+#   echo "%{$fg[yellow]%}%n@%M%{$reset_color%}"
+# }
+
+set_prompt () {
+  export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push) $(rb_prompt)\n%{$fg[red]%}›%{$reset_color%} '
+  # export PROMPT=$'\n$(directory_name) '
+  # show exit code only if it is non-zero
+  RPROMPT='%(?.. %?)'
+}
+
+precmd() {
+  # title "zsh" "%m" "%55<...<%~"
+  set_prompt
+}
--- zshrc
+++ zshrc
@@ -1,32 +1,18 @@
-#    ____   _       ___           __ _
-#   |_  /__| |_    / __|___ _ _  / _(_)__ _
-#    / /(_-< ' \  | (__/ _ \ ' \|  _| / _` |
-#   /___/__/_||_|  \___\___/_||_|_| |_\__, |
-#                                      |___/
+ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="mine"
+plugins=(osx git ruby command-coloring pip)
 
-if [ "$TERM" = "xterm" ]; then
-  if [ -e /usr/share/terminfo/x/xterm-256color ]; then
-    export TERM='xterm-256color'
-  else
-    export TERM='xterm-color'
-  fi
-fi
+export EDITOR='vim'
+export DISABLE_AUTO_TITLE="true"
+export RUBYOPT=rubygems
+export HISTSIZE=1000
+export HISTFILESIZE=1000
+export HISTCONTROL=erasedups
 
-[ -z "$PS1" ] && return
+# Local Settings -------------------------------------------------------------
+if [[ -s $HOME/.zshrc_local ]] ; then source $HOME/.zshrc_local ; fi
 
-. ~/.dotfiles/zsh/completion.zsh
-. ~/.dotfiles/zsh/prompt.zsh
-
-[ -e  ~/.rvm/scripts/rvm ] && . ~/.rvm/scripts/rvm
-
-if [ -e  ~/.rbenv ]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-fi
-
-# Pull in bash functions and aliases to avoid repetation
-source "/Users/jheppler/.dotfiles/bash/functions"
-source "/Users/jheppler/.dotfiles/bash/aliases"
-
-# Don't end with errors.
-true
+source $ZSH/oh-my-zsh.sh
+source $HOME/.dotfiles/bash/aliases
+source $HOME/.dotfiles/bash/functions
+source $HOME/git/se-aliases/se-aliases.sh
