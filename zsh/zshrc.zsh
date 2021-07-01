501d4ce44b4714dcb92b1dc13d2a9f5a0f299cc1
Jason Heppler <jason.heppler@gmail.com>
2021-07-01 15:15:03 -0500

    Finaly pyenv setup.

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -161,3 +161,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
