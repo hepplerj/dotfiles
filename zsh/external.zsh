2b90c9a019f1ea836a85fe2c46f59830a2cd1493
Jason Heppler <jason.heppler@gmail.com>
2022-11-18 14:20:29 -0600

    Don't need fzf in the CLI.

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -3,26 +3,3 @@
 # Autocomplete 
 source <(npm completion)  # adds normal autocomplete to `npm` commands
 eval "$(gh completion -s zsh)"  # adds autocomplete for `gh` tool
-
-# fzf
-# https://github.com/junegunn/fzf
-
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
-# Auto-completion
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-
-# Setting ag as the default source for fzf
-export FZF_DEFAULT_COMMAND='ag -g ""'
-
-# To apply the command to CTRL-T as well
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
-_fzf_compgen_path() {
-  ag -g "" "$1"
-}
