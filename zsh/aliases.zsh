9e527c2dd345ca6c21e5c081833abd77ec0f2ff1
Jason Heppler <jason.heppler@gmail.com>
2023-04-25 09:07:15 -0500

    Quick entry to writing files

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,7 +16,8 @@ alias ip="curl icanhazip.com"       # get current public IP
 alias ls="exa"
 alias more='more -R'                # give more colors
 alias process="ps aux | grep -i"
-alias servethis='python3 -m http.server'
+alias servethis="python3 -m http.server"
+alias writingdir="cd ~/Documents/Research/80.\ Writing"
 
 # Django 
 # -------------------------------------------------------------------
