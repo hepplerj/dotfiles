8b731623c189d707c05b0f9e5d877732217712eb
Jason Heppler <jason.heppler@gmail.com>
2023-12-04 19:20:51 -0500

    Move doom configuration to dotfiles dir

---
+++ emacs/.d.doom/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("~/Library/CloudStorage/Dropbox/org/personal.org" "/Users/jheppler/Dropbox/org/inbox.org" "/Users/jheppler/Dropbox/org/refile.org" "/Users/jheppler/Dropbox/org/agenda.org" "/Users/jheppler/Dropbox/org/work.org" "/Users/jheppler/Dropbox/org/notes.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
