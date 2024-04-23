c1356db9ff5b30be4e2611c74bc963529bcb78cc
Jason Heppler <jason.heppler@gmail.com>
2024-04-23 16:26:42 -0500

    fix: Fixed typo and errors with exa

--- fish/config.fish
+++ fish/config.fish
@@ -10,10 +10,10 @@ set -g theme_hide_hostname no
 set -g theme_hostname always
 
 # aliases
-alias ls "ls -p -G"
-alias la "ls -A"
-alias ll "ls -l"
-alias lla "ll -A"
+alias ls exa
+alias la "ls -a"
+alias ll "exa -l"
+alias lla "ll -a"
 alias g git
 command -qv nvim && alias vim nvim
 
@@ -32,25 +32,24 @@ set -gx PATH $GOPATH/bin $PATH
 
 # NVM
 function __check_rvm --on-variable PWD --description 'Do nvm stuff'
-  status --is-command-substitution; and return
+    status --is-command-substitution; and return
 
-  if test -f .nvmrc; and test -r .nvmrc;
-    nvm use
-  else
-  end
+    if test -f .nvmrc; and test -r .nvmrc
+        nvm use
+    else
+    end
 end
 
 switch (uname)
-  case Darwin
-    source (dirname (status --current-filename))/osx.fish
-  case Linux
-    source (dirname (status --current-filename))/linux.fish
-  case '*'
-    source (dirname (status --current-filename))/windows.fish
+    case Darwin
+        source (dirname (status --current-filename))/osx.fish
+    case Linux
+        source (dirname (status --current-filename))/linux.fish
+    case '*'
+        source (dirname (status --current-filename))/windows.fish
 end
 
 set LOCAL_CONFIG (dirname (status --current-filename))/local.fish
 if test -f $LOCAL_CONFIG
-  source $LOCAL_CONFIG
+    source $LOCAL_CONFIG
 end
-
