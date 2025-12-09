d616610b43c0a7717b6b14d00ab6415ff8c793e9
Jason Heppler <jason.heppler@gmail.com>
2025-12-09 08:51:25 -0600

    Add csvkit to brewfile

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,21 +11,15 @@ alias ....="cd ../../.."
 alias dev="cd /Volumes/Hage/dev/"
 alias home="cd && clear"
 alias findd="find . -type d -iname" # find a directory
-alias findf="find . -type f -iname" # find a file 
+alias findf="find . -type f -iname" # find a file
 alias ip="curl icanhazip.com"       # get current public IP
-alias ls="exa"
+#alias ls="exa"
 alias more='more -R'                # give more colors
 alias process="ps aux | grep -i"
 alias servethis="http-server -p 8000"
 alias writingdir="cd ~/Documents/Research/80.\ Writing"
 
-# Django 
-# -------------------------------------------------------------------
-alias pm="python3 manage.py"
-alias pmm="python3 manage.py migrate"
-alias prs="python3 manage.py runserver"
-
-# Git 
+# Git
 # -------------------------------------------------------------------
 alias ga='git add'
 alias gc='git commit'
@@ -36,7 +30,3 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gs='git status -sb'
 alias gl='git pull --ff-only'
 
-# Conda activation shortcuts
-# -------------------------------------------------------------------
-alias clocal="conda activate local"
-alias cbase="conda activate base"
